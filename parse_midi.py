76748f0be7f47dae7317937a5519ec4bdbdc5bcf
Max Sun <maxlouiesun@gmail.com>
2020-05-08 16:31:47 -0700

    refactored to use only one event type -- simpler code

--- parse_midi.py
+++ parse_midi.py
@@ -31,12 +31,54 @@ class EventType(Enum):
     Channel_Aftertouch = 0xD
     Pitch = 0xE
 
+    Sequence_Num = 0x00
+    Text_Event = 0x01
+    Copyright = 0x02
+    Track_Name = 0x03
+    Instr_Name = 0x04
+    Lyrics = 0x05
+    Marker = 0x06
+    Cue_Point = 0x07
+    Midi_Channel_Prefix = 0x20
+    End_Track = 0x2f
+    Set_Tempo = 0x51
+    SMPTE_Offset = 0x54
+    Time_Sig = 0x58
+    Key_Sig = 0x59
+
     def data_len(self) -> int:
-        if self in [0xC, 0xD]:
+        assert not self.is_meta()
+        if self in [EventType.Program, EventType.Channel_Aftertouch]:
             return 1
         return 2
 
+    def is_meta(self) -> bool:
+        return self not in [
+            EventType.Note_Off,
+            EventType.Note_On,
+            EventType.Note_Aftertouch,
+            EventType.Controller,
+            EventType.Program,
+            EventType.Channel_Aftertouch,
+            EventType.Pitch]
+
     def parse_data(self, data: bytes):
+        if self.is_meta():
+            if self == EventType.SMPTE_Offset:
+                raise NotImplementedError('SPTME Offset Parsing missing!')
+
+            if self == EventType.Time_Sig:
+                return str(data)
+
+            if self in [
+                EventType.Sequence_Num,
+                EventType.Midi_Channel_Prefix,
+                EventType.Set_Tempo
+            ]:
+                return to_int(data)
+            
+            return str(data)
+
         p1, p2 = {
             EventType.Note_Off: ('Note', 'Velocity'),
             EventType.Note_On: ('Note', 'Velocity'),
@@ -55,115 +97,61 @@ class EventType(Enum):
         raise Exception('Error Parsing Data!')
 
 
-class MetaEventType(Enum):
-    Sequence_Num = 0x00
-    Text_Event = 0x01
-    Copyright = 0x02
-    Track_Name = 0x03
-    Instr_Name = 0x04
-    Lyrics = 0x05
-    Marker = 0x06
-    Cue_Point = 0x07
-    Midi_Channel_Prefix = 0x20
-    End_Track = 0x2f
-    Set_Tempo = 0x51
-    SMPTE_Offset = 0x54
-    Time_Sig = 0x58
-    Key_Sig = 0x59
-
-    def parse_data(self, data: bytes):
-
-        if self == MetaEventType.SMPTE_Offset:
-            raise NotImplementedError('SPTME Offset Parsing missing!')
-
-        if self == MetaEventType.Time_Sig:
-            return str(data)
-
-        if self in [
-            MetaEventType.Sequence_Num,
-            MetaEventType.Midi_Channel_Prefix,
-            MetaEventType.Set_Tempo
-        ]:
-            return to_int(data)
-        
-        return str(data)
-
-
-
 class Event(NamedTuple):
     status: EventType
     channel: int
     data: bytes
+    dtime: int
 
     def __repr__(self) -> str:
         params = []
         for byte in self.data:
             params.append(byte)
         x = self.status.parse_data(self.data)
-        return '%s: %s' % (self.status, str(x))
+        return '%s: %s' % (self.status, x)
 
     @staticmethod
-    def from_bytes(b: BufferedReader) -> 'Event':
+    def from_bytes(b: BufferedReader, dtime: int) -> 'Event':
 
-        status = b.read(1)
+        status = to_int(b.read(1))
+        if status == 0xff:
+            status = to_int(b.read(1))
+            msg_len = to_int(b.read(1))
 
-        value = to_int(status) >> 4
+            return Event(
+                EventType(status),
+                0,
+                b.read(msg_len), dtime)
+        value = status >> 4
         return Event(
             EventType(value),
-            to_int(status) % 16,
-            b.read(EventType(value).data_len()))
-
-
-class MetaEvent(NamedTuple):
-    status: MetaEventType
-    data: bytes
-
-    def __repr__(self) -> str:
-        params = []
-        for byte in self.data:
-            params.append(byte)
-        x = self.status.parse_data(self.data)
-        return '%s: %s' % (self.status, str(x))
-
-
-    # TODO: Find out what channel meta events should be on
-
-    @staticmethod
-    def from_bytes(b: BufferedReader) -> 'MetaEvent':
-        meta_flag = to_int(b.read(1))
-        assert meta_flag == 0xff
-        msg_type = to_int(b.read(1))
-        msg_len = to_int(b.read(1))
-        data = b.read(msg_len)
-        return MetaEvent(MetaEventType(msg_type), data)
+            status % 16,
+            b.read(EventType(value).data_len()), dtime)
 
 
 class Track(NamedTuple):
 
     metadata: Dict
-    events: List[Union[Event, MetaEvent]]
+    events: List[Event]
 
     @staticmethod
     def from_bytes(b: BufferedReader) -> 'Track':
         chunk_len = to_int(b.read(4))
         start_pos = b.tell()
-        events: List[Union[Event, MetaEvent]] = []
+        events: List[Event] = []
         while b.tell() - start_pos < chunk_len:
             last_event = events[-1] if len(events) > 0 else None
 
             dtime = read_variable_int(b)
             status_peek = b.peek(1)[:1]
 
-            # print(hex(to_int(status_peek)))
-            if status_peek == b'\xff':
-                events.append(MetaEvent.from_bytes(b))
-            elif status_peek < b'\x80' and last_event and isinstance(last_event, Event):
+            if status_peek < b'\x80' and last_event and isinstance(last_event, Event):
                 events.append(Event(
                     last_event.status,
                     last_event.channel,
-                    b.read(len(last_event.data))))
+                    b.read(len(last_event.data)), dtime))
             else:
-                events.append(Event.from_bytes(b))
+                events.append(Event.from_bytes(b, dtime))
 
         return Track({}, events)
 
@@ -215,7 +203,7 @@ if __name__ == '__main__':
     midi_dir = './midi_files'
     midi_files = [join(midi_dir, x) for x in listdir(midi_dir)]
 
-    file_path = midi_files[9]
+    file_path = midi_files[1]
     with open(file_path, 'rb') as midi_file:
         fio = FileIO(midi_file.fileno())
         midi_buffer = BufferedReader(fio)
