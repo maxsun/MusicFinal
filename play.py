679dc81a834cf7fdeabe58801e9285e59270b4a0
Max Sun <maxlouiesun@gmail.com>
2020-05-08 17:29:32 -0700

    Started adding piano player

---
+++ play.py
@@ -0,0 +1,30 @@
+import mingus.core.notes as notes
+from mingus.containers import Note
+from mingus.midi import fluidsynth
+
+from pathlib import Path
+import time
+# http://bspaans.github.io/python-mingus/
+# Make sure to install FluidSynth: https://github.com/FluidSynth/fluidsynth/wiki/Download
+# OS X: `brew install fluid-synth`
+# Ubuntu/Debian: `sudo apt-get install fluidsynth`
+# Also install SoundFonts, I got one from here: https://rkhive.com/piano.html
+
+cwd = Path.cwd()
+fluidsynth.init(str(cwd / "soundfont.SF2"))
+n = Note("C-5")
+n.channel = 1
+n.velocity = 100
+fluidsynth.play_Note(n)
+
+time.sleep(1)
+fluidsynth.play_Note(Note('A-5'))
+
+# fluidsynth.stop_Note(Note("C-5"), 1)
+
+
+# e = Bar('E', (6, 8))
+# e.place_notes("A-4", 4)
+# fluidsynth.play_Bar(e, 1, 150)
+
+time.sleep(5)
